2854d0ddbea7683bd3bae78bc2c7b68bc9e6012a
Kang-Simon <sangyoon.kang@bosagora.io>
2022-09-13 15:14:08 +0900

    remove translate for turkish language

--- src/intl/index.ts
+++ src/intl/index.ts
@@ -1,17 +1,14 @@
 import en from './compiled/en.json';
 import ko from './compiled/ko.json';
-import tr from './compiled/tr.json';
 
 export const messages: { [key: string]: any } = {
   en,
   ko,
-  tr,
 };
 
 export enum AppLanguage {
   English = 'en',
   Korean = 'ko',
-  Turkish = 'tr',
 }
 
 export const supportedLanguages: String[] = Object.values(AppLanguage);
