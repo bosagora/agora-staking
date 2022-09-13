614d07cd650f0a00a325c279bee2fcf5f2d2e2fa
Kang-Simon <sangyoon.kang@bosagora.io>
2022-09-13 15:45:15 +0900

    remove unnecessary wallet connect

--- src/pages/TopUp/components/WalletConnectModal.tsx
+++ src/pages/TopUp/components/WalletConnectModal.tsx
@@ -8,18 +8,13 @@ import { Layer } from 'grommet';
 import { Network } from 'grommet-icons';
 import {
   AllowedNetworks,
-  fortmatic,
   metamask,
   NetworkChainId,
-  portis,
 } from '../../ConnectWallet/web3Utils';
 import { WalletButton } from '../../ConnectWallet/WalletButton';
 import { web3ReactInterface } from '../../ConnectWallet';
 import metamaskLogo from '../../../static/metamask.svg';
 import closeGlyph from '../../../static/close.svg';
-import { ENABLE_RPC_FEATURES, PORTIS_DAPP_ID } from '../../../utils/envVars';
-import portisLogo from '../../../static/portis.svg';
-import fortmaticLogo from '../../../static/fortmatic.svg';
 import { Heading } from '../../../components/Heading';
 import { Text } from '../../../components/Text';
 import { NakedButton } from '../../../components/NakedButton';
@@ -111,25 +106,6 @@ const WalletConnectModal: React.FC<{
           walletProvider={metamask}
           title="Metamask"
           error={connector === metamask ? error : undefined}
-        />
-        <WalletButton
-          invalid={PORTIS_DAPP_ID === ''}
-          selectedWallet={selectedWallet}
-          setSelectedWallet={setSelectedWallet}
-          logoSource={portisLogo}
-          walletProvider={portis}
-          title="Portis"
-          error={connector === portis ? error : undefined}
-        />
-
-        <WalletButton
-          invalid={!ENABLE_RPC_FEATURES}
-          selectedWallet={selectedWallet}
-          setSelectedWallet={setSelectedWallet}
-          logoSource={fortmaticLogo}
-          walletProvider={fortmatic}
-          title="Fortmatic"
-          error={connector === fortmatic ? error : undefined}
         />
 
         <MetamaskHardwareButton />
